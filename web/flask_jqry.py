6a6ff31607788458facce4e5c963667ea02c4f62
UMGQ <gaoq@umich.edu>
2016-04-13 16:42:10 -0400

    merge

---
+++ web/flask_jqry.py
@@ -0,0 +1,51 @@
+#flask_jqry.py
+import NLP
+import json
+from flask import Flask, request
+from flask import json
+from searcher import Searcher
+from query_speechrecognition import query_speech
+from query import query
+
+app = Flask(__name__)
+
+searcher = None
+
+@app.route('/')
+def main():
+    f = open('index.html', 'r')
+    searcher = Searcher("../data.json")
+
+    return "\"\"\"" + f.read() 
+
+
+@app.route('/process', methods=['POST'])
+def view_do_something():
+
+    if request.method == 'POST':
+        request_text = request.form.get('stext')
+        #location = NLP.do_query_NLP(request_text)
+        #your database process here
+        #data = "hahahah"
+        data = process(request_text)
+        #return "location:" + location
+        return data
+    else:
+        return "NO OK"
+
+def process(q):
+    if not q:
+        return
+    q, filters = query(q)
+    print "Filters:"
+    print filters
+    results = searcher.process_query(q, filters)
+
+    ret = ""
+    for r in results:
+        ret += (str(json.dumps(r)) + "#")
+
+    return ret[:-1]
+
+if __name__ == '__main__':
+    app.run()
