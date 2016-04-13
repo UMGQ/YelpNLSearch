dd64ae475595c9eec377f504d1b5ad2ce9fa740b
Roger-Hong <federer.hyj2721@gmail.com>
2016-04-13 03:39:52 -0400

    google_speech

--- query_speechrecognition.py
+++ query_speechrecognition.py
@@ -27,9 +27,18 @@ def get_audio_query():
 	    print("Say something!")
 	    audio = r.listen(source)
 
-	# recognize speech using google
-	recognized_sentense = r.recognize_google(audio)
-	return recognized_sentense
+	try:
+    	# for testing purposes, we're just using the default API key
+    	# to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    	# instead of `r.recognize_google(audio)`
+    	temp = r.recognize_google(audio)
+    	print("Google Speech Recognition thinks you said " + temp)
+	except sr.UnknownValueError:
+    	print("Google Speech Recognition could not understand audio")
+	except sr.RequestError as e:
+    	print("Could not request results from Google Speech Recognition service; {0}".format(e))
+	#return recognized_sentense
+	return temp
 	'''
 	try:
 	    return r.recognize_sphinx(audio)
